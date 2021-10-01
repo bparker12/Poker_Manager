daa2cbd5056c14b641ea10fe14f89385e5bc7007
bparker12 <49100894+bparker12@users.noreply.github.com>
2021-10-01 07:10:18 -0500

    added react and semantic ui to app

--- src/index.js
+++ src/index.js
@@ -3,12 +3,13 @@ import ReactDOM from 'react-dom';
 import { BrowserRouter as Router } from "react-router-dom"
 import './index.css';
 import App from './App';
+import 'semantic-ui-css/semantic.min.css'
 import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
-  <React.StrictMode>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>,
   document.getElementById('root')
 );
 
